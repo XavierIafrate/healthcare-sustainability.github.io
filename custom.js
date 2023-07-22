1d1540cd5e5f4b1614abdbeb617df6dab1d41ff8
XavierIafrate <xavieriafrate@gmail.com>
2023-07-22 22:50:30 +0100

    started facilities section and improved scroll behaviour

--- custom.js
+++ custom.js
@@ -6,6 +6,28 @@ let processScroll = () => {
       scrollPercent = scrollTop / scrollBottom * 100 + '%';
     
       document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
+
+      revealHighlights();
   }
+
+  async function revealHighlights() {
+    var highlights = document.querySelectorAll('mark');
+    var height = window.innerHeight;
+
+    for(let i = 0; i < highlights.length; i++) {
+        var pos =  highlights[i].getBoundingClientRect().bottom;
+
+
+        if(pos < height - 150 && ! highlights[i].classList.contains('animation')) {
+            await sleep(500)
+            highlights[i].classList.add('animation')
+        }
+    }
+
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
   
   document.addEventListener('scroll', processScroll);
