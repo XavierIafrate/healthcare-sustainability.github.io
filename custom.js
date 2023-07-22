141c51936bf23a39643d0554fa6c0c9e1e8895ea
XavierIafrate <xavieriafrate@gmail.com>
2023-07-22 23:38:49 +0100

    image callout

--- custom.js
+++ custom.js
@@ -8,6 +8,7 @@ let processScroll = () => {
       document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
 
       revealHighlights();
+      revealNumbers()
   }
 
   async function revealHighlights() {
@@ -17,7 +18,6 @@ let processScroll = () => {
     for(let i = 0; i < highlights.length; i++) {
         var pos =  highlights[i].getBoundingClientRect().bottom;
 
-
         if(pos < height - 150 && ! highlights[i].classList.contains('animation')) {
             await sleep(500)
             highlights[i].classList.add('animation')
@@ -26,8 +26,42 @@ let processScroll = () => {
 
   }
 
+  async function revealNumbers() {
+    var height = window.innerHeight;
+    let valueDisplays = document.querySelectorAll(".num");
+valueDisplays.forEach((valueDisplay) => {
+    var pos =  valueDisplay.getBoundingClientRect().bottom;
+
+    if(pos > height - 150) return;
+
+    if(valueDisplay.classList.contains('registered')) return;
+    valueDisplay.classList.add('registered')
+
+
+    let interval = 10
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let increment = endValue / 333;
+
+  let counter = setInterval(function () {
+    startValue += increment;
+    valueDisplay.textContent = startValue.toLocaleString('en-US', {maximumFractionDigits:0});;
+    if (startValue >= endValue) {
+      clearInterval(counter);
+    valueDisplay.textContent = endValue.toLocaleString('en-US', {maximumFractionDigits:0});;
+
+    }
+  }, duration);
+});
+
+  }
+
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
   
+
+
+
   document.addEventListener('scroll', processScroll);
